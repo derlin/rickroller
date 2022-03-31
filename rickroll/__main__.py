9fa88c48d2f73b4434d81042e11b0f43e0b883ee
Lucy Linder <lucy.derlin@gmail.com>
2022-03-31 19:39:43 +0200

    feat: switch to poetry and Docker alpine
    Use poetry to package the project, and a multistage
    docker image based on alpine. The final image serves the app
    with gunicorn.

--- rickroll/__main__.py
+++ rickroll/__main__.py
@@ -1,9 +1,13 @@
 import argparse
-from . import app
 
-if __name__ == '__main__':
+from rickroll import app
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true')
     args = parser.parse_args()
 
     app.run(host='0.0.0.0', port=8080, debug=args.debug)
+
+if __name__ == '__main__':
+    main()
