f536b6a27df56e0c8a78eea447fbd49ba7bb71ba
Lucy Linder <lucy.derlin@gmail.com>
2022-04-03 19:14:07 +0200

    chore: add linter (black)
    This adds a lint workflow (for PRs only) and also runs black
    on all source files.

--- rickroll/__init__.py
+++ rickroll/__init__.py
@@ -4,15 +4,16 @@ import urllib
 
 from .rickroller import RickRoller
 
-app = Flask(__name__, static_folder='assets')
+app = Flask(__name__, static_folder="assets")
 
-@app.route('/')
+
+@app.route("/")
 def rickroll():
-    if (url := request.args.get('u')) is not None:
-        url = urllib.parse.unquote(url) # may be url-encoded
+    if (url := request.args.get("u")) is not None:
+        url = urllib.parse.unquote(url)  # may be url-encoded
         if not urlvalidate(url):
-            return f'Invalid URL {url}'
+            return f"Invalid URL {url}"
 
         return RickRoller.rickroll(url)
 
-    return render_template('index.html')
+    return render_template("index.html")
--- rickroll/__main__.py
+++ rickroll/__main__.py
@@ -2,12 +2,14 @@ import argparse
 
 from rickroll import app
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument("-d", "--debug", action="store_true")
     args = parser.parse_args()
 
-    app.run(host='0.0.0.0', port=8080, debug=args.debug)
+    app.run(host="0.0.0.0", port=8080, debug=args.debug)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
